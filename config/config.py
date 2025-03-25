382d03dc1316abf3434e559473c0469ea0fddeae
pat0nn <phambatrong.pbt@gmail.com>
2025-03-25 12:47:06 +0700

    change source path for kaggle sever

--- config/config.py
+++ config/config.py
@@ -1,12 +1,12 @@
 class Config:
     # Data paths
-    TRAIN_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train_data.json'
-    TEST_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/test_data.json'
-    TRAIN_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train-images'
-    TEST_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/public-test-images'
+    TRAIN_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/train_data.json'
+    TEST_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/test_data.json'
+    TRAIN_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/train-images'
+    TEST_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/public-test-images'
     DATASET_SAVE_PATH = '/run/media/trong/New Volume/Algo/KTVIC/image_caption_dataset'
     
-    GROUNDTRUTH_FILE = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/grouped_captions.json'
+    GROUNDTRUTH_FILE = '/kaggle/working/vietnamese-image-captioning/data/grouped_captions.json'
     
     # Model parameters
     IMAGE_ENCODER_MODEL = "google/vit-large-patch16-224-in21k"
@@ -14,9 +14,9 @@ class Config:
     MAX_TARGET_LENGTH = 64
     
     # Training parameters
-    BATCH_SIZE = 4
-    EVAL_BATCH_SIZE = 4
-    NUM_EPOCHS = 2
+    BATCH_SIZE = 8
+    EVAL_BATCH_SIZE = 8
+    NUM_EPOCHS = 8
     WEIGHT_DECAY = 1e-6
     FP16 = True
     
