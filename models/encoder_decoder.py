ca177eda2abfe4df94ef35157738b7d86019e942
pat0nn <phambatrong.pbt@gmail.com>
2025-03-29 15:25:32 +0700

    use original config

--- models/encoder_decoder.py
+++ models/encoder_decoder.py
@@ -13,27 +13,27 @@ def load_model_and_processors(image_encoder_model, text_decoder_model):
     # Load text tokenizer
     tokenizer = AutoTokenizer.from_pretrained(text_decoder_model)
     
-    # # Configure special tokens
-    # model.config.decoder_start_token_id = tokenizer.bos_token_id
-    # model.config.pad_token_id = tokenizer.pad_token_id
+    # Configure special tokens
+    model.config.decoder_start_token_id = tokenizer.bos_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
     
     
 
-    # Set the decoder start token ID and pad token ID
-    if model.config.decoder_start_token_id is None:
-        model.config.decoder_start_token_id = tokenizer.bos_token_id
+    # # Set the decoder start token ID and pad token ID
+    # if model.config.decoder_start_token_id is None:
+    #     model.config.decoder_start_token_id = tokenizer.bos_token_id
         
     
-    model.generation_config = GenerationConfig(
-        early_stopping=True,
-        no_repeat_ngram_size=3,
-        length_penalty=2.0,
-        num_beams=3,
-        decoder_start_token_id=model.config.decoder_start_token_id,
-        pad_token_id=tokenizer.pad_token_id
-    )
+    # model.generation_config = GenerationConfig(
+    #     early_stopping=True,
+    #     no_repeat_ngram_size=3,
+    #     length_penalty=2.0,
+    #     num_beams=3,
+    #     decoder_start_token_id=model.config.decoder_start_token_id,
+    #     pad_token_id=tokenizer.pad_token_id
+    # )
 
-    model.config.pad_token_id = tokenizer.pad_token_id 
+    # model.config.pad_token_id = tokenizer.pad_token_id 
     
     # Print token info for debugging
     print("EOS token ID:", tokenizer.eos_token_id)
