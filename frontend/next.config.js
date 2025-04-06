5b7c8a2aec710e7959503be047f03716208ebca4
pat0nn <phambatrong.pbt@gmail.com>
2025-04-06 14:54:25 +0700

    final

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,45 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['localhost', 'ngrok.io', 'ngrok-free.app'],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.ngrok-free.app',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.ngrok.io',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'http',
-        hostname: process.env.NEXT_PUBLIC_BACKEND_HOST || 'localhost',
-        port: process.env.NEXT_PUBLIC_BACKEND_PORT || '5000',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_BACKEND_HOST,
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.azureml.ms',
-        pathname: '/uploads/**',
-      },
-    ],
-    unoptimized: true,
+    domains: ['localhost'],
   },
-  output: 'export',
-  trailingSlash: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
